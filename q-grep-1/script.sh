b906b5a30f1496662531f382b4495ab79fc26098
Sayan Ghosh <sgic2001@gmail.com>
2024-03-15 14:05:15 +0530

    Add comments

--- q-grep-1/script.sh
+++ q-grep-1/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Solution 1 ----- faster ------------------------------------
 xargs file -i | grep text/x-shellscript | cut -d":" -f1 | sort
 
+# Solution 2 ----- slower ------------------------------------
 # while read -r line; do
 #   file -i $line | grep text/x-shellscript | cut -d":" -f1
 # done | sort
